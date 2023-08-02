ff066ef8e19de45a64bd962399e0aff4799efd8a
Vy Nguyen <vydpnguyen@gmail.com>
2023-08-02 18:58:20 +0700

    Set up cases for each action type

--- reduxRoad.js
+++ reduxRoad.js
@@ -11,6 +11,20 @@ const reducer = (state = 0, action) => {
                 ...state,
                 supplies: state.supplies + 15,
                 days: state.days + 1
+            };
+        }
+        case 'travel': {
+            return {
+                supplies: state.supplies - (20 * action.payload),
+                distance: state.distance + (10 * action.payload),
+                days: state.days + action.payload
+            }
+        }
+        case 'tippedWagon': {
+            return {
+                ... state,
+                supplies: state.supplies - 30,
+                days: state.days + 1
             }
         }
         default: {
