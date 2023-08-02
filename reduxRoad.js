78036c78f62875344b1172fa24f4c714d6756ec7
Vy Nguyen <vydpnguyen@gmail.com>
2023-08-02 19:16:35 +0700

    Implement extra credit with additional cases

--- reduxRoad.js
+++ reduxRoad.js
@@ -1,7 +1,8 @@
 const initialWagonState = {
     supplies: 100,
     distance: 0,
-    days: 0
+    days: 0,
+    cash: 200
 }
 
 const wagonReducer = (state = initialWagonState, action) => {
@@ -19,6 +20,7 @@ const wagonReducer = (state = initialWagonState, action) => {
             }
             else {
                 return {
+                    ...state,
                     supplies: state.supplies - (20 * action.payload),
                     distance: state.distance + (10 * action.payload),
                     days: state.days + action.payload
@@ -27,9 +29,29 @@ const wagonReducer = (state = initialWagonState, action) => {
         }
         case 'tippedWagon': {
             return {
-                ... state,
+                ...state,
                 supplies: state.supplies - 30,
                 days: state.days + 1
+            }
+        }
+        case 'sell': {
+            return {
+                ...state,
+                supplies: state.supplies - 20,
+                cash: state.cash + 5
+            }
+        }
+        case 'buy': {
+            return {
+                ...state,
+                supplies: state.supplies + 25,
+                cash: state.cash - 15
+            }
+        }
+        case 'theft': {
+            return {
+                ...state,
+                cash: state.cash / 2
             }
         }
         default: {
